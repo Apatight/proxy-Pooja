53c5cb9bc21aeb243474579b5785fd7dbac9db40
Pooja Kodavanti <pkodavan@gmail.com>
2018-05-16 15:07:38 -0700

    Optimize proxy

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+require('newrelic');
 const express = require('express');
-// const morgan = require('morgan');
-const path = require('path');
-
 const app = express();
+const compression = require('compression');
+app.use(compression());
+const path = require('path');
+const cors = require('cors');
+
 const port = 3000;
 
-// app.use(morgan('dev'));
 const bodyParser = require('body-parser');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
+app.use(cors());
 
 
 app.use('/restaurants', express.static(path.join(__dirname, './public')));
@@ -20,6 +24,10 @@ app.get('/restaurants/:id', (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/loaderio-be5347ebeea714e6cba3a81ca4624e10/', (req, res) => {
+  res.send('loaderio-be5347ebeea714e6cba3a81ca4624e10');
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
